b86ea5356e3c43c660c93e7f9fc3cf84d7c31938
Phil Seeley <phil.seeley@gmail.com>
2019-05-12 18:28:12 +1000

    Added peoples names who had some of the ideas.

--- lib/ReleaseNotesPage.dart
+++ lib/ReleaseNotesPage.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class ReleaseNotesPage extends StatelessWidget {
 
   static const changelog = {
-    "6.0.0": "Added options to display the Thermal Updraft Velocity from RASP and show the best direction for each site.",
-    "5.10":  "Fixed group loading issue.",
+    "6.0.0": """Added option to display the Thermal Updraft Velocity from RASP - thanks Dave M for the idea.
+
+Added option to show the best direction for each site - thanks Darryl V for the idea.""",
+    "5.10":  "Fixed group loading issue. Add distance to sites - thanks Greg K for the idea",
     "5.9":   "Pulling down now refeshes the data. We now show the group list instead of the title.",
-    "5.8":   "Added a share button.",
-    "5.7":   "We now allow a site not to have an information page. Tapping the site name now also opens the site page.",
+    "5.8":   "Added a share button - thanks Rob VDK for the idea.",
+    "5.7":   "We now allow a site not to have an information page. Tapping the site name now also opens the site page - thanks Rob VDK for the idea.",
     "5.6":   "Added option to set a default group.",
     "5.5":   "If the location is not available, we now sort by site.",
     "5.4":   "Fixed issue with no forecast being available from BOM.",
@@ -18,7 +20,8 @@ class ReleaseNotesPage extends StatelessWidget {
     "4.0":   "Added groups and option to hide early/late values.",
     "3.0":   "The forecast is now shown even if the location cannot be obtained. Added option to vary icon size.",
     "2.1":   "Added message whilst waiting for data and location.",
-    "2.0":   "Added option to show weather forecats and included weather icons. Added option to show Paraglider values.",
+    "2.0":   "Added option to show weather forecats and included weather icons. Added option to show Paraglider values - thanks Cliff D and Phil L for the idea.",
+    "1.0":   "Thanks to Kimi S for the initial idea.",
   };
 
   final String title;
