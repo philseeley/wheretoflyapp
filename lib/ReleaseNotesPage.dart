0681910013a35cf3b07748bdcf8a013f0c7b6fe6
Phil Seeley <phil.seeley@gmail.com>
2019-09-27 10:04:24 +1000

    Version 6.2.1.

--- lib/ReleaseNotesPage.dart
+++ lib/ReleaseNotesPage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class ReleaseNotesPage extends StatelessWidget {
 
   static const changelog = {
+    "6.2.1": "Fixed hang when location is disabled - thanks to Rob VK for reporting this.",
     "6.2.0": "Thanks to Chris D's excellent work getting http://ausrasp.com up and running, RASP is back!",
     "6.1.1": "Fixed a bug where selecting 'Show Paragliding Values' hides all sites - thanks to Greg K for reporting this.",
     "6.1.0": """Added divider in main list to better show which arrows are for which site - thanks Tushar P for the idea.
