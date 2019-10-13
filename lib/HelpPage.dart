b3647f54b2882597e3977c82ff65c30eca66f679
Phil Seeley <phil.seeley@gmail.com>
2019-10-13 13:20:02 +1100

    Added scale for RASP to the Help page.

--- lib/HelpPage.dart
+++ lib/HelpPage.dart
@@ -24,61 +24,64 @@ class HelpPage extends StatelessWidget {
           )
         ],
       ),
-      body: ListView(children: <Widget>[
-        Row(children: <Widget>[
-          Expanded(child: Icon(Icons.forward, color: Colors.yellow, size: 35.0)),
-          Expanded(child: Icon(Icons.forward, color: Colors.lightGreen, size: 35.0)),
-          Expanded(child: Icon(Icons.forward, color: Colors.orange, size: 35.0)),
-        ]),
-        Row(children: <Widget>[
-          Expanded(child: Text("Too Light", textAlign: TextAlign.center)),
-          Expanded(child: Text("Where To Fly!", textAlign: TextAlign.center)),
-          Expanded(child: Text("Too Strong", textAlign: TextAlign.center)),
-        ]),
-        ListTile(
-          leading: Icon(Icons.gps_fixed),
-          title: Text("Sort sites by location"),
-        ),
-        ListTile(
-          leading: Icon(Icons.power_settings_new, color: Colors.red),
-          title: Text('Only show sites that are "on"'),
-        ),
-        ListTile(
-          leading: Icon(Icons.trending_flat, color: Colors.red),
-          title: Text('Show best direction'),
-        ),
-        ListTile(
-          leading: Icon(Icons.cloud),
-          title: Text('Show text forecast'),
-        ),
-        ListTile(
-          leading: Icon(Icons.blur_on),
-          title: Text('Show RASP Thermal Updraft Velocity'),
-        ),
-        ListTile(
-          leading: Icon(Icons.cloud, color: Colors.red),
-          title: Text('Open realtime weather information'),
-        ),
-        ListTile(
-          leading: Icon(Icons.cloud_upload),
-          title: Text('Open external weather forecast'),
-        ),
-        ListTile(
-          leading: Icon(Icons.info),
-          title: Text('Open external site information'),
-        ),
-        ListTile(
-          leading: Icon(Icons.change_history),
-          title: Text("Show change log"),
-          onTap: () { _showChangelog(context); }
-        ),
-        ListTile(
-          title: Text('Note: your location and other settings are only used and stored within the app and never sent to any external server.')
-        ),
-        ListTile(
-          leading: Icon(Icons.email), title: Text("Tap here to send feedback and site updates"), onTap: _support),
-      ])
-    ));
+      body: Row(children: <Widget>[
+        Expanded(child:
+          ListView(children: <Widget>[
+            Row(children: <Widget>[
+              Expanded(child: Icon(Icons.forward, color: Colors.yellow, size: 35.0)),
+              Expanded(child: Icon(Icons.forward, color: Colors.lightGreen, size: 35.0)),
+              Expanded(child: Icon(Icons.forward, color: Colors.orange, size: 35.0)),
+            ]),
+            Row(children: <Widget>[
+              Expanded(child: Text("Too Light", textAlign: TextAlign.center)),
+              Expanded(child: Text("Where To Fly!", textAlign: TextAlign.center)),
+              Expanded(child: Text("Too Strong", textAlign: TextAlign.center)),
+            ]),
+            ListTile(
+              leading: Icon(Icons.gps_fixed),
+              title: Text("Sort sites by location"),
+            ),
+            ListTile(
+              leading: Icon(Icons.power_settings_new, color: Colors.red),
+              title: Text('Only show sites that are "on"'),
+            ),
+            ListTile(
+              leading: Icon(Icons.trending_flat, color: Colors.red),
+              title: Text('Show best direction'),
+            ),
+            ListTile(
+              leading: Icon(Icons.cloud),
+              title: Text('Show text forecast'),
+            ),
+            ListTile(
+              leading: Icon(Icons.blur_on),
+              title: Text('Show RASP Thermal Updraft Velocity'),
+            ),
+            ListTile(
+              leading: Icon(Icons.cloud, color: Colors.red),
+              title: Text('Open realtime weather information'),
+            ),
+            ListTile(
+              leading: Icon(Icons.cloud_upload),
+              title: Text('Open external weather forecast'),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text('Open external site information'),
+            ),
+            ListTile(
+              leading: Icon(Icons.change_history),
+              title: Text("Show change log"),
+              onTap: () { _showChangelog(context); }
+            ),
+            ListTile(
+              title: Text('Note: your location and other settings are only used and stored within the app and never sent to any external server.')
+            ),
+            ListTile(
+              leading: Icon(Icons.email), title: Text("Tap here to send feedback and site updates"), onTap: _support),
+          ])),
+        Image(image: AssetImage("assets/rasp-scale.png")),
+      ])));
   }
 
   static const mailURL = "xmailto:feedback@${wtfSite}?subject=WTF%20Feedback";
