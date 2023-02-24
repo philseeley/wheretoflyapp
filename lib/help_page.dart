99eca78af5bc21ca12c46c2be3e3bfa6b03797e0
Phil Seeley <phil.seeley@gmail.com>
2023-02-24 18:15:01 +1100

    Removed unused import.

--- lib/help_page.dart
+++ lib/help_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'common.dart';
 import 'data.dart';
 import 'release_notes_page.dart';
